e1588b7fb4c4c04e46624c3316de51bc838cb380
fsanterva <83568459+fsanterva@users.noreply.github.com>
2026-05-06 00:43:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+// serve frontend files
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log("Snake game running"));
+
+app.listen(PORT, () => {
+  console.log("Snake game running on port", PORT);
+});
